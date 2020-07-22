18357590c5d780d543a0b7e9f33d5ae791330204
zandonacamila <zandona.camila@gmail.com>
2020-07-22 14:38:13 -0300

    Module Pattern

---
+++ aluraframe/client/js/app/services/ConnectionFactory.js
@@ -0,0 +1,56 @@
+var ConnectionFactory = (function () {
+    
+    var stores = ['negociacoes'];
+    var version = 4;
+    var dbName = 'aluraframe';
+
+    var connection = null;
+
+    return class ConnectionFactory {
+
+        constructor() {
+
+            throw new Error('Não é possivel criar instâncias de ConnectionFactory');
+        }
+
+        static getConnection() {
+
+            return new Promise((resolve, reject) => {
+
+                let openRequest = window.indexedDB.open(dbName, version);
+
+                openRequest.onupgradeneeded = e => {
+
+                    ConnectionFactory._createStores(e.target.result);            
+                }
+
+                openRequest.onsuccess = e => {
+
+                    if(!connection) connection = e.target.result;
+                    resolve(connection);
+                }
+
+                openRequest.onerror = e => {
+
+                    console.log(e.target.error);
+
+                    reject(e.target.error.name);
+                }
+            });
+        }
+
+        static _createStores(connection) {
+
+            stores.forEach(store => {
+
+                if(connection.objectStoreNames.contains(store)) // sem bloco: a próx. linha está subordinada ao 'if'
+                connection.deleteObjectStore(store);
+
+                connection.crateObjectStore(store, { autoIncrement: true });
+            });
+
+        }
+
+    }
+})();
+
